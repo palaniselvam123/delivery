ac7190dacd8cd2190c54d6957083eff23ad0bfac
Palaniselvam123 <selvamfriendmaker@gmail.com>
2025-10-26 16:27:39 +0530

    Add custom startup.sh for Azure

---
+++ startup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Optional: ensure all dependencies are installed (uncomment the next line if needed)
+# pip install -r requirements.txt
+
+# Start your FastAPI app using gunicorn + uvicorn worker
+exec gunicorn -w 2 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8000 main:app
